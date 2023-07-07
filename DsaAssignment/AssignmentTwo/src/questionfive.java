f72c5a1760bbe4d258e3eae9382813beb425d5d5
Himanshu Saini <55054295+Himanshu-dev-web@users.noreply.github.com>
2023-07-08 02:12:23 +0530

    Update questionfive.java

--- DsaAssignment/AssignmentTwo/src/questionfive.java
+++ DsaAssignment/AssignmentTwo/src/questionfive.java
@@ -1,33 +1,30 @@
-import java.util.Arrays;
-
-public class questionfive {
-    public static void main(String[] args) {
+class Solution {
+   
+    public int maximumProduct(int[] nums) {
+    int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+    int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+    
+    for (int n : nums) {
+        if (n > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = n;
+        } else if (n > max2) {
+            max3 = max2;
+            max2 = n;
+        } else if (n > max3) {
+            max3 = n;
+        }
         
-        int[] num1 = {1,2,3,0,0,0};
-        int[] num2 = {2,5,6};
-        int m=3,n=3;
-
-        int k = (m+n)-1;
-        int a = m-1;
-        int b = n-1;
-
-        while(a >= 0 && b >= 0){
-            if(num1[a] > num2[b]){
-                num1[k--] = num1[a--];
-            }
-            else{
-                num1[k--] = num2[b--];
-            }
+        if (n < min1) {
+            min2 = min1;
+            min1 = n;
+        } else if (n < min2) {
+            min2 = n;
         }
-
-        if(a >= 0){
-            num1[k--] =num1[a--];
-        }
-        if(b >= 0){
-            num1[k--] =num1[b--];
-        }
-
-        System.out.println(Arrays.toString(num1));
-
     }
+    
+    return Math.max(max1 * max2 * max3, max1 * min1 * min2);
+}
+
 }

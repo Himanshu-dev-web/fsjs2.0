2e5803a72f9a8b9537130c15e162ba2a78bcbd23
Himanshu Saini <55054295+Himanshu-dev-web@users.noreply.github.com>
2023-07-08 00:50:51 +0530

    completed questionone.java

--- DsaAssignment/AssignmentTwo/src/questionone.java
+++ DsaAssignment/AssignmentTwo/src/questionone.java
@@ -1,28 +1,13 @@
-import java.util.HashMap;
 
-public class questionone {
-    public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
-        int target = 9;
+class Solution {
+  public int arrayPairSum(int[] nums) {
+    int ans = 0;
 
-        HashMap<Integer, Integer> hash = new HashMap<>();
-        for(int i =0; i < nums.length; i++){
-            hash.put(nums[i],i);
-        }
-        
-         for(int i =0; i < nums.length; i++){
-         int num = nums[i];
-        int rem = target - num;
-             
-             if(hash.containsKey(rem)){
-                 int index = hash.get(rem);
-                 if(index == i)
-                     continue;
-                     else{
-                        System.out.println(i+ "" +index);
-                        break;
-                     }
-             }
-         }
-    }
+    Arrays.sort(nums);
+
+    for (int i = 0; i < nums.length; i += 2)
+      ans += nums[i];
+
+    return ans;
+  }
 }

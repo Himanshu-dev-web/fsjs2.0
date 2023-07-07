d1b71ef38e207e3cbb2177186fa45057ed7bd979
Himanshu Saini <55054295+Himanshu-dev-web@users.noreply.github.com>
2023-07-08 02:18:20 +0530

    Update questioneight.java

--- DsaAssignment/AssignmentTwo/src/questioneight.java
+++ DsaAssignment/AssignmentTwo/src/questioneight.java
@@ -1,31 +1,7 @@
-import java.util.Arrays;
-
-public class questioneight {
-    public static void main(String[] args) {
-        
-
-        int[] nums = {3,2,2};
-
-        int n  =nums.length;
-        int i=0;
-       while(i < n){
-           int correct = nums[i] -1;
-           if(nums[i] != nums[correct]){
-               int temp = nums[i];
-               nums[i] = nums[correct];
-               nums[correct] = temp;
-           }
-           else
-           {
-               i++;
-           }       
-   }
-   System.out.println(Arrays.toString(nums));
-        for(int j=0;j<nums.length;j++){
-           if(nums[j] != j+1){
-               System.out.println(nums[j]+ "" + j+1);
-           }
-       }
-    //    return new int[] {-1,-1};
+class Solution {
+    public int smallestRangeI(int[] A, int k) {
+        Arrays.sort(A);
+        if(A[0]+k >= A[A.length-1]-k)
+            return 0;
+        return A[A.length-1]-k-(A[0]+k);
     }
-}
